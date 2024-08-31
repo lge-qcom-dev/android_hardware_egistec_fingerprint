39781e1191eeba42c2330283966f1db0fa0eaf19
Han Sol Jin <hansol@hansol.ca>
2024-08-31 14:09:43 -0700

    hidl: rbs: Uprev to 2.3
    Change-Id: Iba942bb689a84b699a7c357d756a3c2e806be5bc

--- hidl/rbs/BiometricsFingerprint.cpp
+++ hidl/rbs/BiometricsFingerprint.cpp
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.rbs"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.rbs"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.rbs"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.3-service.rbs"
 
 #include <hardware/hw_auth_token.h>
 
@@ -30,14 +30,11 @@ namespace android {
 namespace hardware {
 namespace biometrics {
 namespace fingerprint {
-namespace V2_1 {
+namespace V2_3 {
 namespace implementation {
 
 // Supported fingerprint HAL version
 static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 1);
-
-using RequestStatus =
-        android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
 
 using ::android::base::StartsWith;
 
@@ -357,6 +354,20 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
             }
             break;
     }
+}
+
+// ::V2_3::IBiometricsFingerprint follow.
+
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
+    return false;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return Void();
 }
 
 } // namespace implementation
